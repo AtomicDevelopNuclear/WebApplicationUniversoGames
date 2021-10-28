903aa03961be1f4216ae46d2eb66459688e59242
git-atomico <gaetano.garofalo1992@gmail.com>
2021-10-28 09:25:30 +0200

    Add new code

---
+++ WebApplicationUniversoGames/Controllers/ReviewsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplicationUniversoGames.Data;
+using WebApplicationUniversoGames.Models;
+
+namespace WebApplicationUniversoGames.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly DataContext _ctx;
+
+        public ReviewsController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_ctx.Reviews);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                review.Date = DateTime.Now;
+                _ctx.Reviews.Add(review);
+                _ctx.SaveChanges();
+                return Redirect("Index");
+            }
+            return View(review);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _ctx.Reviews.Find(id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteReviews(int? id)
+        {
+            var obj = _ctx.Reviews.Find(id);
+            if (obj is null)
+            {
+                return NotFound();
+            }
+            _ctx.Reviews.Remove(obj);
+            _ctx.SaveChanges();
+            return Redirect("Index");
+        }
+    }
+}
