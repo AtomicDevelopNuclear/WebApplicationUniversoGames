9cf715e2fff85f3a17d00b892e18504b01df1573
git-atomico <gaetano.garofalo1992@gmail.com>
2021-10-29 10:19:08 +0200

    Add New Code

--- WebApplicationUniversoGames/Controllers/NewsController.cs
+++ WebApplicationUniversoGames/Controllers/NewsController.cs
@@ -26,7 +26,7 @@ namespace WebApplicationUniversoGames.Controllers
         }
         protected IPagedList<News> GetPagedNames(int? page)
         {
-            // return a 404 if user browses to before the first page
+            // return a 404 if user browses to before the first page value
             if (page.HasValue && page < 1)
                 return null;
 
