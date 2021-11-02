a1b3a44a783791c6d3c146c18547ea6b31cb212c
git-atomico <gaetano.garofalo1992@gmail.com>
2021-11-02 11:55:07 +0100

    Change Code in JS And Change Layout

--- WebApplicationUniversoGames/wwwroot/js/universo.js
+++ WebApplicationUniversoGames/wwwroot/js/universo.js
@@ -1,13 +1,19 @@
 ﻿$(document).ready(
     function () {
+        $("#btn_collapse_menu").hide();
         $("#formnewssearch").hide();
     }
 );
 
 function ShowAndHide() {
+    var navcollapse = document.getElementById('navbarNav');
     var x = document.getElementById('formnewssearch');
     if (x.style.display == 'none') {
-        x.style.display = 'block';
+        if (navcollapse.style.display == 'none') {
+            x.style.marginTop = '35%';
+        } else {
+            x.style.marginTop = '15%';
+        }
     } else {
         x.style.display = 'none';
     }
